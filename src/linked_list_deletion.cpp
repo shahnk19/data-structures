f0625c3deca42ffaaa952c20170a85ca411f1852
shahnk19 <shahnk19@gmail.com>
2021-05-26 13:22:34 +0800

    Implement linked list remove at tail

--- src/linked_list_deletion.cpp
+++ src/linked_list_deletion.cpp
@@ -32,5 +32,49 @@ class LinkedListDeletion : public LinkedList{
       Node *toDelete = pin->next;
       pin->next = toDelete->next;
       delete toDelete;
-    }    
+    }
+
+    void RemoveAtNode(int referenceValue) {
+      Node *pin = head;
+      Node *prev = head;
+
+      // find the Node at requested position
+      while(pin != NULL) {
+        if(pin->value == referenceValue)
+          break;
+        prev = pin;
+        pin = pin->next;
+      }
+
+      if(pin == NULL)
+        return;
+
+      if(pin == head)
+        return Remove();
+
+      // delete the node at the reference Node
+      prev->next = pin->next;
+      delete pin;
+    }
+
+    void RemoveAtTail() {
+      if(IsEmpty())
+        return;
+
+      Node *pin = head;
+      Node *prev = head;
+
+      // find the last Node
+      while(pin->next != NULL) {
+        prev = pin;
+        pin = pin->next;
+      }
+
+      if(pin == head)
+        return Remove();
+
+      // delete the last node
+      prev->next = NULL;
+      delete pin;
+    }
 };
--- test/linked_list_deletion_test.cpp
+++ test/linked_list_deletion_test.cpp
@@ -31,6 +31,47 @@ TEST_F(LinkedListDeletionTest, RemoveAtPosition) {
     EXPECT_EQ ("[ 30 -> 10 ]", ll.Stringify());
 }
 
+TEST_F(LinkedListDeletionTest, RemoveAtNode) {
+    ll.RemoveAtNode(10);
+    EXPECT_EQ ("[]", ll.Stringify());
+    ll.Insert(new Node(10));
+    ll.RemoveAtNode(10);
+    EXPECT_EQ ("[]", ll.Stringify());
+    ll.Insert(new Node(20));
+    ll.Insert(new Node(10));
+    ll.RemoveAtNode(10);
+    EXPECT_EQ ("[ 20 ]", ll.Stringify());
+    ll.Insert(new Node(10));
+    ll.Insert(new Node(5));
+    ll.RemoveAtNode(5);
+    EXPECT_EQ ("[ 10 -> 20 ]", ll.Stringify());
+    ll.Insert(new Node(1));
+    ll.RemoveAtNode(10);
+    EXPECT_EQ ("[ 1 -> 20 ]", ll.Stringify());
+    ll.Insert(new Node(2));
+    ll.RemoveAtNode(20);
+    EXPECT_EQ ("[ 2 -> 1 ]", ll.Stringify());
+}
+
+TEST_F(LinkedListDeletionTest, RemoveAtTail) {
+    ll.RemoveAtTail();
+    EXPECT_EQ ("[]", ll.Stringify());
+    ll.Insert(new Node(10));
+    ll.RemoveAtTail();
+    EXPECT_EQ ("[]", ll.Stringify());
+    ll.Insert(new Node(20));
+    ll.Insert(new Node(10));
+    ll.RemoveAtTail();
+    EXPECT_EQ ("[ 10 ]", ll.Stringify());
+    ll.Insert(new Node(5));
+    ll.Insert(new Node(1));
+    ll.RemoveAtTail();
+    EXPECT_EQ ("[ 1 -> 5 ]", ll.Stringify());
+    ll.Insert(new Node(0));
+    ll.RemoveAtTail();
+    EXPECT_EQ ("[ 0 -> 1 ]", ll.Stringify());
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
